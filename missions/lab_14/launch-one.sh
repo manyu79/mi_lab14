ce334ae4156ccefa30851a6fd12b4a88dc4203cb
dxyazdi <dxyazdi@carma-norway.local>
2013-04-11 09:55:25 -0400

    balls

--- missions/lab_14/launch-one.sh
+++ missions/lab_14/launch-one.sh
@@ -35,7 +35,7 @@ nsplug meta_vehicle.moos targ_$VNAME1.moos -f WARP=$TIME_WARP  \
    VTYPE=UUV          SHORE_LISTEN=$SHORE_LISTEN               \
    MASTER="false"     OFFSET="0"                               \
    WIDTH="5"          PATH_WIDTH="5"                           \
-   PD="0.9"
+   PD="0.95"
 
 
 nsplug meta_vehicle.bhv targ_$VNAME1.bhv -f VNAME=$VNAME1      \

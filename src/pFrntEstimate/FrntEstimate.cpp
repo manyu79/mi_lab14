daacac0cbc19fc24e53365a2f0518e0074227cc3
dxyazdi <dxyazdi@carma-norway.local>
2013-05-14 10:25:44 -0400

    dxyazdi

--- src/pFrntEstimate/FrntEstimate.cpp
+++ src/pFrntEstimate/FrntEstimate.cpp
@@ -66,12 +66,12 @@ CFrntEstimate::CFrntEstimate()
 
   // Weights: These are the weights (relative to 1) for the master (small path) vehicle.
   wt_offset = 0;
-  wt_angle = 1;
+  wt_angle = .75;
   wt_amplitude = 0;
   wt_period = 0;
   wt_wavelength = 0;
   wt_alpha = 1;
-  wt_beta = .85;
+  wt_beta = 1;
   wt_T_N = 1;
   wt_T_S = 1;
   wt_temperature = 1;

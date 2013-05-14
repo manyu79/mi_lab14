446d62780512bd17a0cbf876169849cd9fc51a63
dxyazdi <dxyazdi@carma-norway.local>
2013-05-14 04:46:50 -0400

    dxyazdi

--- src/pFrntEstimate/FrntEstimate.cpp
+++ src/pFrntEstimate/FrntEstimate.cpp
@@ -64,14 +64,14 @@ CFrntEstimate::CFrntEstimate()
   partner_report = false;
   master = true;
 
-  // Weights: These are the weights (relative to 1) for the master vehicle.
-  wt_offset = .5;
+  // Weights: These are the weights (relative to 1) for the master (small path) vehicle.
+  wt_offset = 0;
   wt_angle = 1;
   wt_amplitude = 0;
   wt_period = 0;
   wt_wavelength = 0;
   wt_alpha = 1;
-  wt_beta = .5;
+  wt_beta = 1;
   wt_T_N = 1;
   wt_T_S = 1;
   wt_temperature = 1;
